629bee0df0e1cc4f7849ce3bc84d8a017fe5d20f
gautam-bhagat <gautambhagat.business@gmail.com>
2024-03-08 00:03:15 +0530

    Signup endpoint

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
 const { PORT} = require("./utils/constants")
--- route/auth.js
+++ route/auth.js
@@ -1,4 +1,5 @@
 const bypass = require("../middleware/bypass");
+const AdvisorUser = require("../models/AdvisorUser");
 const {
     hashPassword,matchPassword
 } = require("../utils/services")
@@ -10,6 +11,28 @@ const router = express.Router();
 
 router.get("/",bypass,(req,res)=>{
     res.send("Working")
+})
+
+router.post("/signup",bypass,async (req,res)=>{
+    console.log(req.body)
+    const {name,email,password} = req.body;
+
+    const user = await AdvisorUser.findOne({email:email});
+    console.log(user)
+    if(user){
+        return res.status(303).json({success : 0 , message : "Email already in use!"})
+    }
+
+    const hashPass = await hashPassword(password);
+    const newUser =await  AdvisorUser.create({
+        name,
+        email,
+        password:hashPass
+    })
+
+    console.log(newUser);
+
+    res.status(200).json({ success:1 ,message:"Kindly Verify your email!!"});
 })
 
 module.exports = router
